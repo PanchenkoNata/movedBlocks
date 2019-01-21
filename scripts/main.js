9c79f671dcf0d33c920341b044dc6b3d727ca142
PanchenkoNata <natalitsalucru@gmail.com>
2019-01-21 12:29:34 +0200

    JS add function remove(el)

--- scripts/main.js
+++ scripts/main.js
@@ -2,30 +2,24 @@ const elLeftBlock = document.querySelector( '.leftBlock' );
 const elRightBlock = document.querySelector( '.rightBlock' );
 const elButtonArrows = document.querySelectorAll( '.arrow' );
 
+function remove(el) {
+    const elParentToArrow = el.parentNode;
+
+    if ( el.classList.contains('toRight') ) {
+        elRightBlock.appendChild(elParentToArrow);
+        el.classList.add('toLeft');
+        el.classList.remove('toRight');
+    } else {
+        elLeftBlock.appendChild(elParentToArrow);
+        el.classList.add('toRight');
+        el.classList.remove('toLeft');
+    }
+}
+
 elButtonArrows[0].addEventListener( 'click', (ev) => {
-    const elParentToArrow = elButtonArrows[0].parentNode;
-
-    if ( elButtonArrows[0].classList.contains('toRight') ) {
-        elRightBlock.appendChild(elParentToArrow);
-        elButtonArrows[0].classList.add('toLeft');
-        elButtonArrows[0].classList.remove('toRight');
-    } else {
-        elLeftBlock.appendChild(elParentToArrow);
-        elButtonArrows[0].classList.add('toRight');
-        elButtonArrows[0].classList.remove('toLeft');
-    }
+    remove(elButtonArrows[0]);
 });
-
 elButtonArrows[1].addEventListener( 'click', (ev) => {
-    const elParentToArrow = elButtonArrows[1].parentNode;
-
-    if ( elButtonArrows[1].classList.contains('toRight') ) {
-        elRightBlock.appendChild(elParentToArrow);
-        elButtonArrows[1].classList.add('toLeft');
-        elButtonArrows[1].classList.remove('toRight');
-    } else {
-        elLeftBlock.appendChild(elParentToArrow);
-        elButtonArrows[1].classList.add('toRight');
-        elButtonArrows[1].classList.remove('toLeft');
-    }
+    remove(elButtonArrows[1])
 });
+
