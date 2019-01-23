864f68d0bde4743b7f47ae81b7bdf087e9adc2fd
PanchenkoNata <natalitsalucru@gmail.com>
2019-01-23 13:04:09 +0200

    JS 3-th way from lesson

--- scripts/main.js
+++ scripts/main.js
@@ -1,66 +1,42 @@
-const elButtonArrows = document.querySelectorAll( '.arrow' );
+const el = document.querySelector( '.wrapper' );
 
-function moveToRight(el) {
-    const movedItems = document.querySelectorAll('.moved_item');
-    if ( el.classList.contains('pos_left2')) {
-        el.classList.remove('pos_left2');
-        el.classList.add('pos_right1');
-        return;
+el.addEventListener('click', (ev) => {
+    const elEvent = ev.target;
+    if (!elEvent.classList.contains('live-block')) {
+        return
     }
 
-    let elSubling = movedItems[0];
-    if (elSubling.classList.contains('pos_left1')) {
-        elSubling = movedItems[1];
-    }
-    if ( elSubling.classList.contains('pos_right1')) {
-        el.classList.remove('pos_left1');
-        el.classList.add('pos_right2');
+    let elEventClassL = elEvent.classList;
+	
+	if (elEventClassL.contains('right')) {
+    	if ( document.querySelector('.left')) {
+			elEventClassL.add('left2');
+			elEventClassL.remove('right');
+		} else {
+			elEventClassL.add('left');
+			elEventClassL.remove('right');
+
+			let elSibling = document.querySelector('.right2');
+			elSibling.classList.add('right');
+			elSibling.classList.remove('right2');
+		}
+    } else if ( elEventClassL.contains('left')) {
+		if ( document.querySelector('.right')) {
+			elEventClassL.add('right2');
+			elEventClassL.remove('left');
+		} else {
+			elEventClassL.add('right');
+			elEventClassL.remove('left');
+
+			let elSibling = document.querySelector('.left2');
+			elSibling.classList.add('left');
+			elSibling.classList.remove('left2');
+		}
+	} else if (elEventClassL.contains('right2')) {
+    	elEventClassL.add('left');
+		elEventClassL.remove('right2');
     } else {
-        el.classList.remove('pos_left1');
-        el.classList.add('pos_right1');
-        elSubling.classList.remove('pos_left2');
-        elSubling.classList.add('pos_left1');
+    	elEventClassL.add('right');
+		elEventClassL.remove('left2');
     }
-}
-function moveToLeft(el) {
-    const movedItems = document.querySelectorAll('.moved_item');
-    if ( el.classList.contains('pos_right2')) {
-        el.classList.remove('pos_right2');
-        el.classList.add('pos_left1');
-        return;
-    }
-    let elSubling = movedItems[0];
-    if (elSubling.classList.contains('pos_right1')) {
-        elSubling = movedItems[1];
-    }
-    if ( elSubling.classList.contains('pos_left1')) {
-        el.classList.remove('pos_right1');
-        el.classList.add('pos_left2');
-    } else {
-        el.classList.remove('pos_right1');
-        el.classList.add('pos_left1');
-        elSubling.classList.remove('pos_right2');
-        elSubling.classList.add('pos_right1');
-    }
-}
-
-function move(el) {
-    const elParentToArrow = el.parentNode;
-
-    if ( el.classList.contains('toRight') ) {
-        moveToRight(elParentToArrow);
-        el.classList.add('toLeft');
-        el.classList.remove('toRight');
-    } else {
-        moveToLeft(elParentToArrow);
-        el.classList.add('toRight');
-        el.classList.remove('toLeft');
-    }
-}
-
-elButtonArrows[0].addEventListener( 'click', (ev) => {
-    move(elButtonArrows[0]);
-});
-elButtonArrows[1].addEventListener( 'click', (ev) => {
-    move(elButtonArrows[1])
 });
